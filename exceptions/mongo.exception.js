cfbcdc71c3675d34a4bbde24762b53e5521aac99
Orel Balilti <orelba93@gmail.com>
2018-10-26 15:13:50 +0300

    add support for  multiple record update

---
+++ exceptions/mongo.exception.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@sugoi/core");
+class SugoiMongoError extends core_1.GenericException {
+}
+exports.SugoiMongoError = SugoiMongoError;
---
+++ interfaces/mongo-connection-config.interface.d.ts
@@ -0,0 +1,5 @@
+import { IConnectionConfig } from "@sugoi/orm";
+export interface IMongoConnectionConfig extends IConnectionConfig {
+    newParser?: boolean;
+    protocol: string;
+}
