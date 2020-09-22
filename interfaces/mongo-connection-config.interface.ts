3024f41bd551721f4bfb89e7a106aa6f6da36a50
Orel Balilti <orelba93@gmail.com>
2020-09-22 20:10:23 +0300

    optional protocol for cloud support

--- interfaces/mongo-connection-config.interface.ts
+++ interfaces/mongo-connection-config.interface.ts
@@ -2,5 +2,5 @@ import {IConnectionConfig} from "@sugoi/orm";
 
 export interface IMongoConnectionConfig extends IConnectionConfig{
     newParser?:boolean
-    protocol:string
+    protocol?:string
 }
